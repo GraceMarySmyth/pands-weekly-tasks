958f980f16b8d91d6389e57763d3215cb0fe52b0
Grace Mary Smyth <157698316+GraceMarySmyth@users.noreply.github.com>
2024-03-06 10:46:32 +0000

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,10 @@
+# Weekly tasks 02
+# Python bank.py
+# Author: Grace Mary Smyth
+# Reference PerpBytes Blog, Ankit Kochar(19/11/2022)
+
+x = int(input("Enter amount1(in cent):"))
+
+y = int( input("Enter amount2(in cent):"))
+sum = sum([x,y])/100
+print("Total: $", sum)
